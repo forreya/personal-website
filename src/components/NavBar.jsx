d66e5292466437368873cc20a67496104560b747
Ryan Lai <ryanlai12@gmail.com>
2023-04-16 20:11:04 +0100

    Completed underline function + added icons to Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,20 @@
 import Link from "next/link"
 import Logo from "./Logo"
+import { useRouter } from "next/router"
+import { GitHubIcon, LinkedInIcon } from "./Icons"
 
 const CustomLink = ({href, title, className=""}) => {
+  const router = useRouter()
+
   return (
     <Link href={href} className={`${className} relative group`}>
       {title}
-      <span className="h-[2px] inline-block w-0 bg-dark absolute left-0 -bottom-1 group-hover:w-full transition-[width] ease duration-300">&nbsp;</span>
+      <span className={`
+      h-[1px] inline-block bg-dark absolute 
+      left-0 -bottom-1 group-hover:w-full 
+      transition-[width] ease duration-300
+      ${router.asPath === href ? 'w-full' : 'w-0'}
+      `}>&nbsp;</span>
     </Link>
   )
 }
@@ -22,9 +31,12 @@ const NavBar = () => {
         <CustomLink href="/articles" title="Articles"className="ml-4"/>
       </nav >
       <nav>
-        <Link href="/" target={"_blank"} className="mx-4">GitHub</Link>
-        <Link href="/" target={"_blank"} className="ml-4">LinkedIn</Link>
-        <Link href="/" target={"_blank"}></Link>
+        <Link href="/" target={"_blank"} className="mx-4">
+          <GitHubIcon />
+        </Link>
+        <Link href="/" target={"_blank"} className="ml-4">
+          <LinkedInIcon />
+        </Link>
       </nav>
       
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
