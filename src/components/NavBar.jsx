346c58742b72f169f7fcf1cb0f3ba72027e4d7b1
Ryan Lai <ryanlai12@gmail.com>
2023-04-25 13:34:20 +0100

    Added motion scaling for hamburger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -87,7 +87,10 @@ const NavBar = () => {
 
     {
       isOpen ?
-      <div className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+      <motion.div 
+      initial={{scale:0, opacity:0, x: "-50%", y: "-50%"}}
+      animate={{scale:1, opacity:1}}
+      className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
       bg-dark/90 rounded-lg backdrop-blur-md py-32
       ">
         <nav className="flex items-center flex-col justify-center">
@@ -113,7 +116,7 @@ const NavBar = () => {
             <LinkedInIcon /> 
           </motion.a>
         </nav>
-      </div>
+      </motion.div>
       :
         null
     }
