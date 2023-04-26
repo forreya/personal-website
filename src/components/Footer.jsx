e525f1b96f94186d3f41c5582ed8490ed0d84331
Ryan Lai <ryanlai12@gmail.com>
2023-04-26 15:37:45 +0100

    Made paragraph, image & numbers in AboutMe page responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="w-full border-t-2 border-solid border-dark
     font-medium text-lg sm:text-base h-full p-0">
-      <Layout className="py-8 flex items-center justify-between xl:py-8 lg:flex-col lg:py-2 md:py-3">
+      <Layout className="py-8 flex items-center justify-between xl:!py-8 lg:flex-col lg:!py-2 md:!py-3">
           <span className="">Hello there</span>
           <Link href="/" className="my-2">Made by Ryan Lai</Link>
           <Link href="/">This is my website</Link>
