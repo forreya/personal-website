5bee864adc031b238ee377c3078df05d2558fe08
Ryan Lai <ryanlai12@gmail.com>
2023-05-02 14:48:35 +0800

    Switched logo to next/image

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,12 @@
 import Link from "next/link"
+import Image from "next/image"
 import { motion } from 'framer-motion'
+import yingYang from "public/ying-yang.png"
 
 const MotionLink = motion(Link);
 
 const Logo = () => {
+  console.log(yingYang)
   return (
     <div className="flex items-center justify-center mt-2">
       <MotionLink 
@@ -12,7 +15,12 @@ const Logo = () => {
           scale:1.2,
         }}
       >
-          <img src={'/ying-yang.png'} alt="image" className="w-16 h-16" />
+        <Image src={yingYang}
+        alt="image" 
+        className="w-16 h-16"
+        height="512"
+        width="512"
+        />
       </MotionLink>
     </div>
   )
